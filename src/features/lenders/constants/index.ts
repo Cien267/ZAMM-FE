50fdd4f10ab1adb958b6fb3dc28d8ddc41a5c24d
Cien267 <kiennguyenduc267@gmail.com>
2026-01-11 22:28:12 +0700

    chore: interest rate constant

--- src/features/lenders/constants/index.ts
+++ src/features/lenders/constants/index.ts
@@ -11,33 +11,32 @@ export const VALIDATION = {
     RATE_MIN: 0,
     RATE_MAX: 100,
   },
-};
+}
 
-export const RATE_TYPES = [
-  "Standard Variable",
-  "Fixed 1 Year",
-  "Fixed 2 Years",
-  "Fixed 3 Years",
-  "Fixed 4 Years",
-  "Fixed 5 Years",
-  "Comparison Rate",
-  "Interest Only Variable",
-  "Introductory Rate",
-  "Other",
-] as const;
+export const OWNER_OCCUPIED_PRINCIPAL_INTEREST = 'OOPI'
+export const OWNER_OCCUPIED_INTEREST_ONLY = 'OOIO'
+export const INVESTMENT_PRINCIPAL_INTEREST = 'IVPI'
+export const INVESTMENT_INTEREST_ONLY = 'IVIO'
+
+export const INTEREST_RATE_TYPES = [
+  OWNER_OCCUPIED_PRINCIPAL_INTEREST,
+  OWNER_OCCUPIED_INTEREST_ONLY,
+  INVESTMENT_PRINCIPAL_INTEREST,
+  INVESTMENT_INTEREST_ONLY,
+] as const
 
 export const lenderKeys = {
-  all: ["lenders"] as const,
-  lists: () => [...lenderKeys.all, "list"] as const,
+  all: ['lenders'] as const,
+  lists: () => [...lenderKeys.all, 'list'] as const,
   list: (query: any) => [...lenderKeys.lists(), query] as const,
-  details: () => [...lenderKeys.all, "detail"] as const,
+  details: () => [...lenderKeys.all, 'detail'] as const,
   detail: (id: string) => [...lenderKeys.details(), id] as const,
-};
+}
 
 export const loanKeys = {
-  all: ["loans"] as const,
-  lists: () => [...loanKeys.all, "list"] as const,
+  all: ['loans'] as const,
+  lists: () => [...loanKeys.all, 'list'] as const,
   list: (query: any) => [...loanKeys.lists(), query] as const,
-  details: () => [...loanKeys.all, "detail"] as const,
+  details: () => [...loanKeys.all, 'detail'] as const,
   detail: (id: string) => [...loanKeys.details(), id] as const,
-};
+}
